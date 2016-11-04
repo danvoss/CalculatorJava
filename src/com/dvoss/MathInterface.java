e594c75cb33032b7043c681c651c4102952650d3
Dan <danielandrewvoss@gmail.com>
2016-11-04 14:47:52 -0400

    Refactor and correct compile errors

---
+++ src/com/dvoss/MathInterface.java
@@ -0,0 +1,16 @@
+package com.dvoss;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * Created by Dan on 11/4/16.
+ */
+public interface MathInterface extends Remote {
+
+    int add(int a,int b) throws RemoteException;
+    int subt(int a,int b) throws RemoteException;
+    int mult(int a,int b) throws RemoteException;
+    int div(int a,int b) throws RemoteException;
+
+}
---
+++ src/com/dvoss/MathServer.java
@@ -0,0 +1,50 @@
+package com.dvoss;
+
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+/**
+ * Created by Dan on 11/4/16.
+ */
+
+/*
+* debugging from source code at https://blog.udemy.com/java-projects/
+*/
+
+public class MathServer extends UnicastRemoteObject implements MathInterface {
+
+    public MathServer() throws RemoteException {
+        System.out.println("Initializing Server");
+    }
+    public int add(int a, int b) {
+        return(a+b);
+    }
+    public int subt(int a, int b) {
+        return(a-b);
+    }
+    public int mult(int a, int b) {
+        return(a*b);
+    }
+    public int div(int a, int b) {
+        return(a/b);
+    }
+
+    public static void main(String args[]) {
+
+        try {
+            MathServer ms = new MathServer();
+            java.rmi.Naming.rebind("MathServ", ms);
+            System.out.println("Server Ready");
+        }
+
+        catch(RemoteException RE) {
+            System.out.println("Remote Server Error: "+ RE.getMessage());
+            System.exit(0);
+        }
+
+        catch(MalformedURLException ME) {
+            System.out.println("Invalid URL");
+        }
+    }
+}
